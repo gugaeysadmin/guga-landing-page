adf462b1a474658c2856a984aae19f05ffd4fa69
gugaeysadmin <gugaeysadmn@gmail.com>
2024-10-22 16:24:17 -0600

    changing header

--- resources/views/layouts/landingpage.blade.php
+++ resources/views/layouts/landingpage.blade.php
@@ -31,6 +31,16 @@
             <main>
                 {{ $slot }}
             </main>
+
+            <!-- Page Footer -->
+            @isset($footer)
+                <footer class="bg-white dark:bg-gray-800 shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $footer }}
+                    </div>
+                </footer>
+            @endisset 
+
         </div>
     </body>
 </html>
