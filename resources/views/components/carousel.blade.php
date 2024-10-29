9e0ccebcc1ed69a078a8a77c1fdd4b056f8e7ebe
gugaeysadmin <gugaeysadmn@gmail.com>
2024-10-29 16:17:42 -0600

    custom carousel

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -3,7 +3,7 @@
 <div>
     <div
         id="carouselExampleCaptions"
-        class="relative"
+        class="relative min-h-[90vh]"
         data-twe-carousel-init
         data-twe-ride="carousel">
         <!--Carousel indicators-->
@@ -16,7 +16,7 @@
                 data-twe-target="#carouselExampleCaptions"
                 data-twe-slide-to={{ $index }}
                 {{ $loop->first ? 'data-twe-carousel-active' : '' }}
-                class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+                class="mx-[8px] box-content h-[23px] w-[23px] flex-initial  rounded-full cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
                 aria-current={{ $loop->first ? 'true' : 'false' }}
                 aria-label={{ $index }}></button>
             @endforeach
@@ -24,7 +24,7 @@
 
         <!--Carousel items-->
         <div
-            class="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
+            class="relative w-full block min-h-[90vh]  overflow-hidden after:clear-both after:block after:content-['']">
 
             @foreach ($images as $index => $image )
                 <div
@@ -34,7 +34,7 @@
                     style="backface-visibility: hidden">
                         <img
                             src={{  $image['image'] }}
-                            class="block w-full"
+                            class="block h-[90vh] object-cover"
                             alt="..." />
                         {{-- <div
                             class="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
@@ -45,7 +45,7 @@
         </div>
 
         <!--Carousel controls - prev item-->
-        <button
+        {{-- <button
             class="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
             type="button"
             data-twe-target="#carouselExampleCaptions"
@@ -68,10 +68,10 @@
             class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
             >Previous</span
             >
-        </button>
+        </button> --}}
         
         <!--Carousel controls - next item-->
-        <button
+        {{-- <button
             class="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
             type="button"
             data-twe-target="#carouselExampleCaptions"
@@ -94,6 +94,6 @@
             class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
             >Next</span
             >
-        </button>
+        </button> --}}
     </div>
 </div>
